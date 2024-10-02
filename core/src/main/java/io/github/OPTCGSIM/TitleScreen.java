68c0fe5fe3c2ff9f793c6ec158e5fc3591a9090d
Jared K <jarod137@nmsu.edu>
2024-10-02 10:45:45 -0600

    added dependancies

--- core/src/main/java/io/github/OPTCGSIM/TitleScreen.java
+++ core/src/main/java/io/github/OPTCGSIM/TitleScreen.java
@@ -2,13 +2,18 @@ package io.github.OPTCGSIM;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class TitleScreen implements Screen {
 
     @Override
     public void show() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'show'");
+        super.show();
     }
 
     @Override
