9af9ca09988645530067ff463129be165f5af2f2
David <davidkuntz02@gmail.com>
2024-10-11 12:43:34 -0600

    logpose.java i thought i did this already

--- core/src/main/java/io/github/OPTCGSIM/LogPose.java
+++ core/src/main/java/io/github/OPTCGSIM/LogPose.java
@@ -1,24 +1,27 @@
 package io.github.OPTCGSIM;
 
+//Main networking component of the sim. All networking variables and methods are found here
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Net.*;
 import com.badlogic.gdx.net.*;
 
 //peer 2 peer server functions
 public class LogPose {
-    //we can declare all connection variables here and use them elsewhere maybe
-    public static Protocol protocol;
-    public static int port, connect;
-    public static ServerSocket server;
-    Socket socket;
-    ServerSocketHints s_hints;
-    SocketHints c_hints;
-    String host;
+    //we can declare all connection variables here and use them elsewhere if needed
+    public static Protocol lp_protocol;
+    public static int lp_port, lp_connect;
+    public static ServerSocket lp_server;
+    public static Socket lp_socket;
+    public static ServerSocketHints lp_s_hints;
+    public static SocketHints lp_c_hints;
+    public static String lp_host;
+
     //what functions do we need
 
     //start server
     //this is how you would start the server on your end
-    //server = LogPose.startMatch(LogPose.protocol,port,s_hints);
+    //server = LogPose.startMatch(protocol,port,s_hints);
     public static ServerSocket startMatch(Protocol protocol, int port, ServerSocketHints hints){
         ServerSocket server = Gdx.net.newServerSocket(protocol, port, hints);
         return server;
@@ -29,9 +32,12 @@ public class LogPose {
         return socket;
     }
     //seek available servers
+    public static int joinMatch(){
+        return 1;
+    }
     //establish connection
     //send turn moves
-    //receive turn moves
+    //receive turn moves (will need to parse JSON...)
         //differentiating the type of turn should happen within the send/receive turn methods
         //attack turn
         //defend turn
