4216b758950ca3507289eadc0d493bef7daf4100
Alex <78886791+alexander2475@users.noreply.github.com>
2024-10-18 13:31:06 -0600

    Completed web scraper. Need docker to run. Docker may report errors. Working on it.

--- WebScraper/webTest.py
+++ WebScraper/webTest.py
@@ -2,24 +2,56 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import os
-import getpass
 from PIL import Image
 from concurrent.futures import ThreadPoolExecutor
+import time
+
+from selenium import webdriver
+from selenium.common import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver import ActionChains
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
 
 URL = "https://en.onepiece-cardgame.com/cardlist/"
-page = requests.get(URL, headers=headers)
 
-soup = BeautifulSoup(page.content, "html.parser")
+driver = webdriver.Chrome()
+driver.set_page_load_timeout(20)
+
+try:
+    print(f"Getting {URL}")
+    driver.get(URL)
+    print(f"Got {URL}")
+
+    cookie_button = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "onetrust-close-btn-handler"))
+    )
+    cookie_button.click()
+    print("Cookie button clicked!")
+
+except TimeoutException:
+    print(f"Timeout while loading {URL}")
+
+page_source = driver.page_source
+
+# --- BeautifulSoup Part ---
+soup = BeautifulSoup(page_source, "html.parser")
 results = soup.find(class_="resultCol")
+options = soup.find(class_="searchCol")
+
+select_element = driver.find_element(By.ID, "series")
+select_obj = Select(select_element)
 
 card_elements = results.find_all("dl", class_="modalCol")
 card_images = results.find_all("a", class_="modalOpen")
 
 cards = []
+img_urls = []
 
 def isEmpty(item):
     if item is None or len(item) == 0:
@@ -66,57 +98,109 @@ class Card:
             "imgPath": self.img
         }
 
-    def to_string(self):
-        return f"{self.name} \n {self.cost} \n {self.power} \n {self.counter} \n {self.color} \n {self.type} \n {self.effect} \n {self.set} \n {self.attribute} \n {self.count} \n {self.img} \n"
-
 cardCount = 1
 
-for card in card_elements:
-    title_element = card.find("div", class_="cardName")
-    cost_element = card.find("div", class_="cost")
-    power_element = card.find("div", class_="power")
-    counter_element = card.find("div", class_="counter")
-    color_element = card.find("div", class_="color")
-    type_element = card.find("div", class_="feature")
-    effect_element = card.find("div", class_="text")
-    info_element = card.find("div", class_="getInfo")
-    attribute_element = card.find("div", class_="attribute")
+try:
+    options = driver.find_elements(By.XPATH, "//li[@class='selModalClose']")
+    print(len(options) - 1)
 
-    newCard = Card(
-        isEmpty(title_element),
-        isEmpty(cost_element).replace("Cost", ""),
-        isEmpty(power_element).replace("Power", ""),
-        isEmpty(counter_element).replace("Counter", ""),
-        isEmpty(color_element).replace("Color", ""),
-        isEmpty(type_element).replace("Type", ""),
-        isEmpty(effect_element),
-        isEmpty(info_element),
-        isEmpty(attribute_element).replace("Attribute", ""),
-        cardCount,
-        "OnePieceSim/assets/" + str(cardCount) + ".jpg"
+except Exception as e:
+    print(f"Error: {e}")
+
+optionDropDown = WebDriverWait(driver, 20).until(
+    EC.element_to_be_clickable((By.CLASS_NAME, "selModalButton"))
+)
+
+print("Clicking dropdown click")
+optionDropDown.click()
+
+for i in range(2, len(options) - 2):
+    print(f"--Viewing fresh sheet--")
+
+    options = driver.find_elements(By.XPATH, "//li[@class='selModalClose']")
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, "html.parser")
+    results = soup.find(class_="resultCol")
+    card_elements = results.find_all("dl", class_="modalCol")
+    card_images = results.find_all("a", class_="modalOpen")
+
+    for card in card_elements:
+        title_element = card.find("div", class_="cardName")
+        cost_element = card.find("div", class_="cost")
+        power_element = card.find("div", class_="power")
+        counter_element = card.find("div", class_="counter")
+        color_element = card.find("div", class_="color")
+        type_element = card.find("div", class_="feature")
+        effect_element = card.find("div", class_="text")
+        info_element = card.find("div", class_="getInfo")
+        attribute_element = card.find("div", class_="attribute")
+
+        newCard = Card(
+            isEmpty(title_element),
+            isEmpty(cost_element).replace("Cost", ""),
+            isEmpty(power_element).replace("Power", ""),
+            isEmpty(counter_element).replace("Counter", ""),
+            isEmpty(color_element).replace("Color", ""),
+            isEmpty(type_element).replace("Type", ""),
+            isEmpty(effect_element),
+            isEmpty(info_element),
+            isEmpty(attribute_element).replace("Attribute", ""),
+            cardCount,
+            "OnePieceSim/assets/" + str(cardCount) + ".jpg"
+        )
+
+        cards.append(newCard)
+        cardCount += 1
+
+    for idx, images in enumerate(card_images, start=len(img_urls)+1):
+        image = images.find("img", class_="lazy")
+        image_link = image["data-src"]
+        full_img_url = URL.rstrip('/cardlist') + image_link.replace("..", "")
+        img_urls.append((idx, full_img_url))
+
+    # with ThreadPoolExecutor() as executor:
+    #     executor.map(download_image, img_urls)
+
+    print(f"Visible: {options[i].is_displayed()}")
+
+    if options[i].is_displayed() == True:
+        print("Detected as true")
+        options[i].click()
+        time.sleep(2)
+
+    search_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//*[@id=\"cardlist\"]/main/article/div/div[1]/form/div[3]/input"))
     )
 
-    cards.append(newCard)
-    cardCount = cardCount + 1
+    print(f"Visible: {search_button.is_displayed()}")
+    search_button.click()
+    # time.sleep(2)
 
-img_urls = []
+    optionDropDown = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "selModalButton"))
+    )
 
-for idx, images in enumerate(card_images, start=1):
-    image = images.find("img", class_="lazy")
-    image_link = image["data-src"]
-    full_img_url = URL.rstrip('/cardlist') + image_link.replace("..", "")
-    img_urls.append((idx, full_img_url))
+    optionDropDown.click()
+    print(f"Dropdown visible: {optionDropDown.is_displayed()}")
 
-os.chdir("../assets/cards/")
-with ThreadPoolExecutor() as executor:
-    executor.map(download_image, img_urls)
-
-#for card in cards:
-#    print(card.to_string() + "\n")
+    # time.sleep(2)
+    print("Finished round")
 
 cards_json = [card.to_dict() for card in cards]
 
-os.chdir("../../WebScraper/")
+# os.chdir("../../WebScraper")
 
 with open("data.json", 'w+', encoding='utf-8') as file:
     json.dump(cards_json, file, ensure_ascii=False, indent=4)
+
+os.chdir("../assets/cards/")
+
+print(f"Amount of cards: {len(cards)}")
+print(f"Amount of card images: {len(img_urls)}")
+
+with ThreadPoolExecutor() as executor:
+    executor.map(download_image, img_urls)
+
+print("Scraping complete.")
+time.sleep(10)
+driver.quit()
